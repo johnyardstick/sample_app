91f7702d4c5675367960f942c4f5d9ca47208848
John Holmes <johnh@getyardstick.com>
2017-01-25 00:26:05 -0500

    login is now recognized

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,17 @@
 module SessionsHelper
+
+  # Log in given user
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # returns current logged in user if they exist
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # returns true if the user is logged in
+  def logged_in?
+    !current_user.nil?
+  end
 end
