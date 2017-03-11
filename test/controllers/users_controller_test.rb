a7a0d5c3113f563ec2349c2404d9efffe4295bef
John Holmes <johnh@getyardstick.com>
2017-03-11 01:47:33 -0500

    delete users as admin

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -8,7 +8,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should redirect index when not logged in" do
-    get users_path
+  get users_path
     assert_redirected_to login_url
   end
 
@@ -44,5 +44,20 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert flash.empty?
     assert_redirected_to root_url
   end
+
+  test "should redirect destroy when not logged in" do
+  assert_no_difference 'User.count' do
+    delete user_path(@user)
+  end
+  assert_redirected_to login_url
+end
+
+test "should redirect destroy when logged in as a non-admin" do
+  log_in_as(@other_user)
+  assert_no_difference 'User.count' do
+    delete user_path(@user)
+  end
+  assert_redirected_to root_url
+end
 
 end
